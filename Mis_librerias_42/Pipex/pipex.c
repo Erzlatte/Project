b682f2c370b0b29fa9c02e016114fc8d11654c18
Erzlatte <lleradld@gmail.com>
2024-06-27 10:53:49 +0200

    21

--- Mis_librerias_42/Pipex/pipex.c
+++ Mis_librerias_42/Pipex/pipex.c
@@ -6,83 +6,63 @@
 /*   By: dllera-d <dllera-d@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 10:18:54 by dllera-d          #+#    #+#             */
-/*   Updated: 2024/06/19 10:26:11 by dllera-d         ###   ########.fr       */
+/*   Updated: 2024/06/22 13:32:11 by dllera-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "extras.h"
+#include "pipex.h"
 
-char	**command(int argc, const char *argv[], int lc)
+void	exec(char *cmd, char **env)
 {
-	char	**cadena;
+	char	**s_cmd;
+	char	*path;
 
-	argc = 0;
-	cadena = ft_split(argv[2 + lc], ' ');
-	while (cadena[argc])
+	s_cmd = ft_split(cmd, ' ');
+	path = get_path(s_cmd[0], env);
+	if (execve(path, s_cmd, env) == -1)
 	{
-		printf("%s\n", cadena[argc]);
-		argc++;
+		ft_putstr_fd("pipex: command not found: ", 2);
+		ft_putendl_fd(s_cmd[0], 2);
+		ft_free_tab(s_cmd);
+		exit(0);
 	}
-
-	if (longcd(cadena) != 0)
-		return (NULL);
-
-	return (cadena);
 }
 
-void	incmd(int fd[2], char **c1)
+void	child(char **av, int *p_fd, char **env)
 {
-	dup2(fd[1], STDOUT_FILENO);
-	close(fd[0]);
-	close(fd[1]);
-	execlp(c1[0], c1[0], c1[1], c1[2]);
+	int		fd;
+
+	fd = open_file(av[1], 0);
+	dup2(fd, 0);
+	dup2(p_fd[1], 1);
+	close(p_fd[0]);
+	exec(av[2], env);
 }
 
-void	outcmd(int fd[2], char **c1)
+void	parent(char **av, int *p_fd, char **env)
 {
-	int		fd1;
+	int		fd;
 
-	fd1 = open(FILE_NAME, O_WRONLY);
-	dup2(fd[0], STDIN_FILENO);
-	close(fd[0]);
-	close(fd[1]);
-	dup2(fd1, STDOUT_FILENO);
-	execlp(c1[0], c1[0], c1[1], c1[2]);
+	fd = open_file(av[4], 1);
+	dup2(fd, 1);
+	dup2(p_fd[0], 0);
+	close(p_fd[1]);
+	exec(av[3], env);
 }
 
-int	cmd(int argc, const char *argv[], int fd[2])
+int	main(int ac, char **av, char **env)
 {
-	char	**c1;
-	int		pid2;
-	int		pid1;
+	int		p_fd[2];
+	pid_t	pid;
 
-	c1 = command(argc, argv, 0);
-	pid1 = fork();
-	if (pid1 < 0 || c1 == NULL)
-		return (1);
-	if (pid1 == 0)
-		incmd(fd, c1);
-	c1 = command(argc, argv, 1);
-	pid2 = fork();
-	if (pid2 < 0 || c1 == NULL)
-		return (1);
-	if (pid2 == 0)
-		outcmd(fd, c1);
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
-	return (0);
-}
-
-int	pipex(int argc, const char *argv[])
-{
-	int		fd[2];
-
-	if (pipe(fd) == -1)
-		return (1);
-
-	cmd(argc, argv, fd);
-
-	return (0);
+	if (ac != 5)
+		error_exit(1);
+	if (pipe(p_fd) == -1)
+		exit(-1);
+	pid = fork();
+	if (pid == -1)
+		exit(-1);
+	if (!pid)
+		child(av, p_fd, env);
+	parent(av, p_fd, env);
 }
